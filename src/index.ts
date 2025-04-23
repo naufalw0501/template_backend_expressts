5dd5488b2e9a68e8c15dc0abe0fa6bd7f6030104
unknown <naufal.wijaya9a@gmail.com>
2025-04-23 22:38:25 +0700

    change cors to app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import app from './app';
-import cors from "cors";
 require('dotenv').config();
 const PORT = process.env.APP_PORT || 3000;
 const MODE: "DEVELOPMENT" | "PRODUCTION" = process.env.APP_MODE === "DEVELOPMENT" ? "DEVELOPMENT" : "PRODUCTION";
 
-app.use(cors({
-  origin: "http://localhost:4000", // sesuaikan dengan frontend kamu
-  credentials: true                // ini penting kalau pakai cookie atau ingin kirim credentials
-}));
 
 app.listen(PORT, () => {
   console.log(`Server Running On ${MODE} Mode With Port ${PORT}`);
